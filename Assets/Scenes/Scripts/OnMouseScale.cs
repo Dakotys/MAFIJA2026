9e481712d6fe2462528474564ce01826e01d9e60
Matus <ggezmasta99@gmail.com>
2026-02-06 01:12:57 +0100

    scaletest

---
+++ Assets/Scenes/Scripts/OnMouseScale.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class OnMouseScale : MonoBehaviour
+{
+    public void PointerEnter()
+    {
+        transform.localScale = new Vector2(1.2f, 1.2f);
+    }
+
+
+    public void PointerExit()
+    {
+        transform.localScale = new Vector2(1f, 1f);
+    }
+}
